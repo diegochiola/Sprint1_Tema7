b3a1be40146fd62ebb79aeff56dce17c66a5f48f
diega <diegochiola@gmail.com>
2024-01-07 23:09:45 +0100

    correcion de texto en ejercicio 5 php basic

--- Nivel1/Ejercicio2/tests/unit/VerificarGrado.php
+++ Nivel1/Ejercicio2/tests/unit/VerificarGrado.php
@@ -6,11 +6,11 @@ $nota = 0;
 
 function gradoEstudiante(int $nota): string {
     if ($nota >= 60){
-        $respuesta = "Primera División";
+        $respuesta = "Primera Division";
     }else if($nota >= 45 && $nota <= 59){
-        $respuesta = "Segunda División";
+        $respuesta = "Segunda Division";
     }else if($nota > 33 && $nota <= 44){
-        $respuesta = "Tercera División";
+        $respuesta = "Tercera Division";
     }else{
         $respuesta = "Reprobado";
     }
