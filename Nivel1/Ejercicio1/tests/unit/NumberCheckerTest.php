a6b8ca539d0a3c9d7d0334527ce68e76db7d837c
diega <diegochiola@gmail.com>
2024-01-07 15:23:19 +0100

    Ejercicio1 Tema7 inicio de tests

--- Nivel1/Ejercicio1/tests/unit/NumberCheckerTest.php
+++ Nivel1/Ejercicio1/tests/unit/NumberCheckerTest.php
@@ -4,16 +4,23 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
+//Clase para poder hacer el testeo
 final class NumberCheckerTest extends TestCase{
     #[Test]
     #[TestDox('Test para corroborar si el numero es par')]
     public function test_isEven():void{
+        
+        $numeroPar = new NumberChecker(8);
+        $numeroImpar = new NumberChecker(21);
+
+        $this->assertTrue($numeroPar->isEven());
+        $this->assertFalse($numeroImpar->isEven());
 
     }
     #[Test]
     #[TestDox('Test para corroborar que el numero seapositivo')]
     public function test_isPositive():void{
-        
+
     } 
 }
 
